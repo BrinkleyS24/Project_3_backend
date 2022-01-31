85f117220e573a1edb18fffe8e11cef8852e919d
Stacey Brinkley <brinkleystacey12@gmail.com>
2022-01-31 10:23:07 -0500

    name required

--- models/meals.js
+++ models/meals.js
@@ -1,11 +1,11 @@
 const mongoose = require('mongoose');
 
 const mealsSchema = new mongoose.Schema({
-    recipieName: String,
+    recipieName: {type: String, required: true},
     recipieThumb: String,
     price: Number,
     qty: Number,
-})
+}, {timestamps: true})
 
 const Meals = mongoose.model('Meals', mealsSchema);
 
