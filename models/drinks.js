b81b05f161e69557a3b7a9f6a04c2871d1891271
Stacey Brinkley <brinkleystacey12@gmail.com>
2022-02-02 19:57:41 -0500

    number to strings

--- models/drinks.js
+++ models/drinks.js
@@ -3,8 +3,8 @@ const mongoose = require('mongoose')
 const drinksSchema = new mongoose.Schema({
     name: {type: String, required: true},
     image: String,
-    price: String,
-    quantity: String,
+    price: Number,
+    quantity: Number,
 }, {timestamps: true})
 
 const Drinks = mongoose.model('Drinks', drinksSchema)
